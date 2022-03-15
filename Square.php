68c7404e00705f38b43c27ad75c86da39636f551
andrikshumit@gmail.com <andrikshumit@gmail.com>
2022-03-15 20:09:26 +0300

    add declare

--- Square.php
+++ Square.php
@@ -1,16 +1,19 @@
 <?php
-    namespace Timush\Square;
 
-    class Square{
+declare(strict_types=1);
 
-        public $val = 1;
-        public function __construct($val = 1) {
-            $this->val = $val;
-            echo "This is construct<br>";
-        }
+namespace Timush\Square;
 
-        public function getVaue() {
-            return $this->val * $this->val;
-        }
+class Square{
+
+    public $val = 1;
+    public function __construct($val = 1) {
+        $this->val = $val;
+        echo "This is construct<br>";
     }
+
+    public function getVaue() {
+        return $this->val * $this->val;
+    }
+}
 ?>
